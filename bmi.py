9aa0838fb969a35e55b72ae2e2016c23ab31521e
Art Rookijakers <rookart253@gmail.com>
2024-04-25 11:11:26 +0800

    BMI Calculator

---
+++ bmi.py
@@ -0,0 +1,14 @@
+def calculate_bmi(height, weight):     
+    print("Height = " + str(height)) 
+    print("Weight = " + str(weight)) 
+    bmi=(weight/height**2)
+    print(str(bmi))
+    if bmi < 18.5:
+       print ("You are Underweight.")
+    elif 18.5 <= bmi <= 25.0:
+        print ("You are of Normal Weight")
+    elif 25.0 < bmi :
+        print ("You are Overweight.")
+
+calculate_bmi(weight=57, height=1.73) 
+
