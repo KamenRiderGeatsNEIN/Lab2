9f5aa717712b8cfb7d84417b930caca5cb2f2bcb
Art Rookijakers <rookart253@gmail.com>
2024-05-02 10:39:55 +0800

    (Return Zero)

--- bmi.py
+++ bmi.py
@@ -4,10 +4,14 @@ def calculate_bmi(height, weight):
     bmi=(weight/height**2)
     print(str(bmi))
     if bmi < 18.5:
+       result = -1
        print ("You are Underweight.")
     elif 18.5 <= bmi <= 25.0:
+        result = 0
         print ("You are of Normal Weight")
     elif 25.0 < bmi :
+        result = 1
         print ("You are Overweight.")
+    return result
 
 
